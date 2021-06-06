9ac2dacf64107f0fb9084ebafbdb0e069877b621
namanmehta284 <85451544+namanmehta284@users.noreply.github.com>
2021-06-06 23:10:21 +0530

    Add files via upload

---
+++ Apps.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function Apps() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default Apps;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Apps from "./components/Apps";
+
+ReactDOM.render(
+  <div>
+    <Apps />
+  </div>,
+  document.getElementById("root")
+);
